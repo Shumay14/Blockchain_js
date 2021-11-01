20efaa99a61493a555f8fe019a2bd8c289ea1f47
Shumay14 <seo533481@naver.com>
2021-11-01 17:08:33 +0900

    .

--- DID_test/migrations/2_deploy_contracts.js
+++ DID_test/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Faucet = artifacts.require("./Faucet.sol");
+var createDID = artifact.require("./registryDID.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Faucet);
+  deployer.deploy(registryDID);
 };
--- DID_test/models/createDID.js
+++ DID_test/models/createDID.js
@@ -1,33 +1,32 @@
-import { TokenSigner, createUnsecuredToken, decodeToken } from "jsontokens"
- 
-const rawPrivateKey = '278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f'
-const tokenPayload = {"iat": 1440713414.85}
-const token = new TokenSigner('ES256K', rawPrivateKey).sign(tokenPayload)
-
-const unsecuredToken = createUnsecuredToken(tokenPayload)
-
-const tokenData = decodeToken(token)
-
-const { Sidetree, MnemonicKeySystem } = require("@transmute/element-lib");
-
-// Instantiate the Sidetree class
-const element = new Sidetree(/* See previous section for how to initialize the Sidetree class*/);
-
 // Generate a simple did document model
-const mks = new MnemonicKeySystem(MnemonicKeySystem.generateMnemonic());
-const primaryKey = await mks.getKeyForPurpose("primary", 0);
-const recoveryKey = await mks.getKeyForPurpose("recovery", 0);
-const didDocumentModel = element.op.getDidDocumentModel(
+const didDocumentModel = getDidDocumentModel(
   primaryKey.publicKey,
-  recoveryKey.publicKey
+  recoveryKey.privateKey
 );
 
 // Generate Sidetree Create payload
-const createPayload = element.op.getCreatePayload(didDocumentModel, primaryKey);
+const createPayload = getCreatePayload(didDocumentModel, primaryKey);
 
 // Create the Sidetree transaction.
 // This can potentially take a few minutes if you're not on a local network
-const createTransaction = await element.batchScheduler.writeNow(createPayload);
-const didUniqueSuffix = element.func.getDidUniqueSuffix(createPayload);
-const did = `did:elem:ropsten:${didUniqueSuffix}`;
+
 console.log(`${did} was successfully created`);
+
+
+const getDidDocumentModel = (primaryPublicKey, recoveryPrivateKey) => ({
+  '@context': 'https://w3id.org/did/v1',
+  publicKey: [
+    {
+      id: '#primary',
+      usage: 'signing',
+      type: 'Secp256k1VerificationKey2018',
+      publicKeyHex: primaryPublicKey,
+    },
+    {
+      id: '#recovery',
+      usage: 'recovery',
+      type: 'Secp256k1VerificationKey2018',
+      publicKeyHex: recoveryPublicKey,
+    },
+  ],
+});
