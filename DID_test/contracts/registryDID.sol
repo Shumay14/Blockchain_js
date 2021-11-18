b1e695ada34988f4d22f111d30f419926ba66c12
Shumay14 <seo533481@naver.com>
2021-11-18 23:51:43 +0900

    s

--- DID_test/contracts/registryDID.sol
+++ DID_test/contracts/registryDID.sol
@@ -1,27 +1,34 @@
 pragma solidity >=0.4.22 <0.9.0;
 
+
 contract registryDID {
 
-  address owner;
-  uint owner_id;
-
-  address[] public dids;
-
-  mapping(address => getDID) public registeredDid;
-
-  // get enhashed data
-  struct getDID {
-      bytes32 encryptName;
-      bytes32 encryptAge;
-      bytes32 encryptMender;
-      bytes32 encryptMarried;  
+  struct repositoryVC {
+      string nameVC;
+      string genderVC;
+      string ageVC;
   }
+
+  struct  didInfo {
+      uint countRepoVC;
+      repositoryVC[] repoVC;
+  }
+
+
+
+  mapping (address => didInfo) ownerVC;
+
+  function registerVC (string memory _nameVC, string memory _genderVC, string memory _ageVC) public {
+
+    ownerVC[msg.sender].countRepoVC++;
+    
+    ownerVC[msg.sender].repoVC.push(repositoryVC(_nameVC, _genderVC, _ageVC));
   
-  function setAddress (address _address) public {      
-        dids.push(_address);
-        owner_id = dids.length;
   }
 
 
-
+function getVC (address _add, uint _num) public view returns(repositoryVC memory) {
+   return ownerVC[_add].repoVC[_num];
+}
+  
 }
